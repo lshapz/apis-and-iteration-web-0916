49f40b83819b2fd7cf6ee465fa97c9214aedc4fb
Craig Iturbe <craigiturbe@Craigs-MacBook-Air.local>
2016-09-28 15:13:13 -0400

    add character specific

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -2,35 +2,23 @@ require 'pry'
 require 'rest-client'
 require 'JSON'
 
-def get_character_movies_from_api(character)
-  #make the web request
+
+def all_character_hash
   all_characters = RestClient.get('http://www.swapi.co/api/people/')
-  character_hash = JSON.parse(all_characters)
-
-  indiv_char = character_hash.fetch("results").find {|char_hash|   
-char_hash.fetch("name").downcase == character
-# binding.pry
-} 
-    
-  film_array = indiv_char.fetch("films")
- names_array = film_array.collect do |x| 
-    film_json =  RestClient.get(x)
-    film_regular =   JSON.parse(film_json)
-
-end 
-
-  # iterate ove the character hash to find the collection of `films` for the given
-  #   `character`
-  # collect those film API urls, make a web request to each URL to get the info
-  #  for that film
-  # return value of this method should be collection of info about each film. 
-  #  i.e. an array of hashes in which each hash reps a given film
-  # this collection will be the argument given to `parse_character_movies`
-  #  and that method will do some nice presentation stuff: puts out a list 
-  #  of movies by title. play around with puts out other info about a given film.
+  JSON.parse(all_characters)
 end
 
-# puts get_character_movies_from_api("r2-d2")
+def get_character_info(character)
+    indiv_char = all_character_hash.fetch("results").find {|char_hash| char_hash.fetch("name").downcase == character.downcase}
+end
+
+def get_character_specific(character, trait)
+  get_character_info(character).fetch(trait)
+end
+
+puts get_character_specific("R2-D2", "hair_color")
+
+
 
 def get_movie_info(film)
  all_films = RestClient.get('http://www.swapi.co/api/films/')
@@ -40,7 +28,7 @@ def get_movie_info(film)
 
 end
 
-# p get_movie_info("A New Hope")
+
 
 def parse_movie(movie)
   movie.each do |x|
@@ -49,7 +37,7 @@ def parse_movie(movie)
   return nil
 end 
 
-puts parse_movie(get_movie_info("A New Hope"))
+#puts parse_movie(get_movie_info("A New Hope"))
 
 def parse_character_movies(films_hash)
   # binding.pry
